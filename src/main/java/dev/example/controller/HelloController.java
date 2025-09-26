971559ca37764206bb96b02a776a43005fcc1a6c
yuunseo <dbstj7457@naver.com>
2025-09-26 16:44:38 +0900

    한번더

--- src/main/java/dev/example/controller/HelloController.java
+++ src/main/java/dev/example/controller/HelloController.java
@@ -3,6 +3,7 @@ package dev.example.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Date; // 불필요한 import 또는 미사용 클래스
 
 @RestController
 @RequestMapping("/api/hello")
@@ -10,6 +11,13 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "Hello YUNSEO!!"
+        String responseMessage = "Hello YUNSEO!! The time is: " + new Date().toString();
+
+        return responseMessage;
+    }
+
+    @GetMapping(params = "version=2")
+    public String hello(int dummyParam) {
+        return "Hello YUNSEO, Version 2!";
     }
 }
